4203570f92b5746931cfb1e74f6bfcf32df4bc30
MichaelJSwain <42355296+MichaelJSwain@users.noreply.github.com>
2023-09-28 06:05:05 +0200

    Add 2 subviews

--- UIKitPractise/ViewController.swift
+++ UIKitPractise/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         view.backgroundColor = UIColor.magenta
+        
+        let myView = UIView(frame: CGRect(x: 50, y: 50, width: 200, height: 100))
+        myView.backgroundColor = UIColor.blue
+        myView.clipsToBounds = true
+        
+        let myView2 = UIView(frame: CGRect(x: 50, y: 50, width: 200, height: 100))
+        myView2.backgroundColor = UIColor.yellow
+        
+        view.addSubview(myView)
+        myView.addSubview(myView2)
     }
 
 
