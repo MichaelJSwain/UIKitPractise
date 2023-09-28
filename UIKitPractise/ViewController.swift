8d352cbd4aeea0a5e2ed7210392f206003911804
MichaelJSwain <42355296+MichaelJSwain@users.noreply.github.com>
2023-09-28 06:51:29 +0200

    Add subviews and image

--- UIKitPractise/ViewController.swift
+++ UIKitPractise/ViewController.swift
@@ -23,6 +23,29 @@ class ViewController: UIViewController {
         
         view.addSubview(myView)
         myView.addSubview(myView2)
+        
+        let myView3 = UIView(frame: CGRect(x: 100, y: 300, width: 200, height: 300))
+        myView3.backgroundColor = UIColor.systemGreen
+        view.addSubview(myView3)
+        
+        let title = UILabel(frame: CGRect(x: 10, y: 10, width: 100, height: 100))
+        title.text = "Hello!"
+        title.textColor = UIColor.red
+        
+        let image = UIImageView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+        image.image = UIImage(named: "IMG1")
+        
+        let contentStack = UIStackView(frame: CGRect(x: 50, y: 50, width: 100, height: 100))
+        contentStack.addArrangedSubview(title)
+        contentStack.addArrangedSubview(image)
+        contentStack.axis = NSLayoutConstraint.Axis.vertical
+        contentStack.alignment = UIStackView.Alignment.center
+        
+        contentStack.backgroundColor = .yellow
+        
+        myView3.addSubview(contentStack)
+//        myView3.addSubview(image)
+        
     }
 
 
